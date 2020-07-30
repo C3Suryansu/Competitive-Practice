f59a0651c8304fc6bf6a902f4f684e44741357d1
C3Suryansu <suryansudash5@gmail.com>
2020-07-30 11:25:09 +0530

    Create Make Readable.py

---
+++ Codewars/Make Readable.py
@@ -0,0 +1,7 @@
+def make_readable(seconds):
+    minutes = seconds // 60
+    hours = minutes // 60
+    seconds -= minutes * 60
+    minutes -= hours * 60
+    res = str(hours).rjust(2, '0') + ":" + str(minutes).rjust(2, '0') + ":" + str(seconds).rjust(2, '0')
+    return res
