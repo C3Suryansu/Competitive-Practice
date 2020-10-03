ab7d8fbcdb841cbc8edafcbe5f60121bb8576c77
Sobhit25 <64360724+Sobhit25@users.noreply.github.com>
2020-10-03 13:35:01 +0530

    Update Week2.py
    This is my code for week2

--- NPTEL/DSA/Week2.py
+++ NPTEL/DSA/Week2.py
@@ -1,59 +1,80 @@
-def isPrime(n):
-    if (n <= 1) :  
-        return False
-    if (n <= 3) :  
-        return True
-    if (n % 2 == 0 or n % 3 == 0) :  
-        return False 
-    i = 5
-    while(i * i <= n) :  
-        if (n % i == 0 or n % (i + 2) == 0) :  
-            return False
-        i = i + 6
-    return True
-
-
-def primeproduct(m):
-    if m > 0:
-        i=2
-        k=0
-        while i**2<=m:
-            if m%i==0:
-                if not (isPrime(i) and isPrime(m/i)):
-                    k=-1
-                    break
-            i=i+1
-        if k==-1:
-            return False
-        elif k==0:
-            return True
-    else:
-        return False
-#------------------------------------
-def delchar(s, c):
-    if len(c)==1:
-        return s.replace(c,'')
-    else:
+def primeproduct(n):
+    i=2
+    while i<n:
+        if ifprime(i):
+            if n%i == 0:
+                if ifprime(n/i):
+                    return True
+        i=i+1
+    return False 
+  
+def ifprime(k):
+    p=2
+    if (k==1):
+        return 0
+    while p<k:
+        if k%p == 0:
+            return 0
+        else:
+            if p<(k/p):
+                p=p+1
+            else: 
+                break
+    return 1  
+  
+  
+def delchar(s,c):
+    a= ""
+    if len(c) != 1:
         return s
+    else:
+        for char in s:
+            if char != c:
+                 a = a + char
+        return a
+      
+      
+      
+      
+def shuffle(l1,l2):
+    ln = [] 
+    i = 0
+    if (len(l1)>len(l2)):
+        t = len(l1)
+    else:
+        t = len(l2)
+    while(i < t ):
+        if i < len(l1):
+            ln.append(l1[i])
+        if i < len(l2):
+            ln.append(l2[i])
+        i =i + 1   
+    return ln  
+import ast
 
-#-------------------------------------
+def tolist(inp):
+  inp = "["+inp+"]"
+  inp = ast.literal_eval(inp)
+  return (inp[0],inp[1])
 
-def shuffle(l1, l2):
-  l3 = []
-  if len(l1) == len(l2):
-    for i in range(len(l1)):
-      l3.append(l1[i])
-      l3.append(l2[i])
-  elif len(l1) < len(l2):
-    for i in range(len(l1)):
-      l3.append(l1[i])
-      l3.append(l2[i])
-    for i in l2[len(l1):]:
-      l3.append(i)
-  else:
-    for i in range(len(l2)):
-      l3.append(l1[i])
-      l3.append(l2[i])
-    for i in l1[len(l2):]:
-      l3.append(i)
-  return l3
+def parse(inp):
+  inp = ast.literal_eval(inp)
+  return (inp)
+
+fncall = input()
+lparen = fncall.find("(")
+rparen = fncall.rfind(")")
+fname = fncall[:lparen]
+farg = fncall[lparen+1:rparen]
+
+if fname == "primeproduct":
+   arg = parse(farg)
+   print(primeproduct(arg))
+elif fname == "delchar":
+   (s1,s2) = parse(farg)
+   print(delchar(s1,s2))
+elif fname == "shuffle":
+   (l1,l2) = parse(farg)
+   print(shuffle(l1,l2))
+else:
+   print("Function", fname, "unknown")
