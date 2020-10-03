b3443e0472f0c7a4d35e06b307d36ffb57453b06
Sumanpal3108 <64361664+Sumanpal3108@users.noreply.github.com>
2020-10-03 13:42:37 +0530

    Update Week2 Suman.py

--- NPTEL/DSA/Week2.py
+++ NPTEL/DSA/Week2.py
@@ -1,59 +1,75 @@
-def isPrime(n):
-    if (n <= 1) :  
-        return False
-    if (n <= 3) :  
-        return True
-    if (n % 2 == 0 or n % 3 == 0) :  
-        return False 
-    i = 5
-    while(i * i <= n) :  
-        if (n % i == 0 or n % (i + 2) == 0) :  
+def is_prime(n):
+    for i in range(2, n):
+        if n % i == 0:
             return False
-        i = i + 6
     return True
 
-
-def primeproduct(m):
-    if m > 0:
-        i=2
-        k=0
-        while i**2<=m:
-            if m%i==0:
-                if not (isPrime(i) and isPrime(m/i)):
-                    k=-1
-                    break
-            i=i+1
-        if k==-1:
-            return False
-        elif k==0:
-            return True
-    else:
+def primeproduct(n):
+    flag=0
+    if n<0 :
         return False
-#------------------------------------
-def delchar(s, c):
-    if len(c)==1:
-        return s.replace(c,'')
-    else:
+    p=[2]
+
+    for i in range(3,n):
+        if(is_prime(i)):
+            p.append(i)
+    for i in p:
+        for j in p:
+            if(i*j == n):
+                flag=1
+                return True
+    if(flag==0):
+        return False
+
+def delchar(s,c):
+    if(len(c)>1):
         return s
+    return(s.replace(c,''))
 
-#-------------------------------------
+def shuffle(a,b):
+    c=[]
+    n1=len(a)
+    n2=len(b)
+    if(n1>n2):
+        n=n2
+    else:
+        n=n1
+        
+    for i in range(0,n):
+        c.append(a[i])
+        c.append(b[i])
 
-def shuffle(l1, l2):
-  l3 = []
-  if len(l1) == len(l2):
-    for i in range(len(l1)):
-      l3.append(l1[i])
-      l3.append(l2[i])
-  elif len(l1) < len(l2):
-    for i in range(len(l1)):
-      l3.append(l1[i])
-      l3.append(l2[i])
-    for i in l2[len(l1):]:
-      l3.append(i)
-  else:
-    for i in range(len(l2)):
-      l3.append(l1[i])
-      l3.append(l2[i])
-    for i in l1[len(l2):]:
-      l3.append(i)
-  return l3
+    if(n1!=n2):
+        if(n1>n2):
+            c=c+a[n:]
+        else:
+            c=c+b[n:]
+    return(c)
+import ast
+
+def tolist(inp):
+  inp = "["+inp+"]"
+  inp = ast.literal_eval(inp)
+  return (inp[0],inp[1])
+
+def parse(inp):
+  inp = ast.literal_eval(inp)
+  return (inp)
+
+fncall = input()
+lparen = fncall.find("(")
+rparen = fncall.rfind(")")
+fname = fncall[:lparen]
+farg = fncall[lparen+1:rparen]
+
+if fname == "primeproduct":
+   arg = parse(farg)
+   print(primeproduct(arg))
+elif fname == "delchar":
+   (s1,s2) = parse(farg)
+   print(delchar(s1,s2))
+elif fname == "shuffle":
+   (l1,l2) = parse(farg)
+   print(shuffle(l1,l2))
+else:
+   print("Function", fname, "unknown")
